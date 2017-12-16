7aa277036e2bef0239dbf8723a227f942487ede3
ipd <ipd@91792-HE139>
2017-12-16 15:14:43 -0500

    Front end merged * car types and care pricing now retreived from DB/Model

--- CarSareMerged/CarSareMerged/Controllers/HomeController.cs
+++ CarSareMerged/CarSareMerged/Controllers/HomeController.cs
@@ -3,14 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CarSareMerged.Models;
+using CarSareMerged.ViewModels;
+
 
 namespace CarSareMerged.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            HomePageViewModel indexViewModel = new HomePageViewModel(db.CarTypes.ToList());
+            return View(indexViewModel);
         }
 
         public ActionResult About()
---
+++ CarSareMerged/CarSareMerged/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using CarSareMerged.Models;
+
+namespace CarSareMerged.ViewModels
+{
+    public class HomePageViewModel
+    {
+
+        public List<CarType> lstCarTypes { get; set; }
+
+        public HomePageViewModel(List<CarType> carTypes)
+        {
+            lstCarTypes = carTypes;
+        }
+    }
+
+}
