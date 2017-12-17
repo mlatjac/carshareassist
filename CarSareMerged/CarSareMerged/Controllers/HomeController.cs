2755a2ba2ad1212d845186fb5aee282a56a0a97c
MihoLaptop <@home>
2017-12-16 19:22:20 -0500

    merge conflict erased

--- CarSareMerged/CarSareMerged/Controllers/HomeController.cs
+++ CarSareMerged/CarSareMerged/Controllers/HomeController.cs
@@ -4,26 +4,22 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CarSareMerged.Models;
-<<<<<<< HEAD
-=======
+
 using CarSareMerged.ViewModels;
 
->>>>>>> 458104a9a893f02334f0b9637d64cd4d39a60d58
 
 namespace CarSareMerged.Controllers
 {
     public class HomeController : Controller
     {
-<<<<<<< HEAD
         private ApplicationDbContext _context;
 
         public HomeController()
         {
             _context = new ApplicationDbContext();
         }
-=======
-        private ApplicationDbContext db = new ApplicationDbContext();
->>>>>>> 458104a9a893f02334f0b9637d64cd4d39a60d58
+        
+
 
         public ActionResult Index()
         {
