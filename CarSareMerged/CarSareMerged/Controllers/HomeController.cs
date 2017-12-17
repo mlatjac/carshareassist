7b68192c11d639a8d0054bbdf8340a5521a33fc9
MihoLaptop <@home>
2017-12-16 19:13:47 -0500

    no message

--- CarSareMerged/CarSareMerged/Controllers/HomeController.cs
+++ CarSareMerged/CarSareMerged/Controllers/HomeController.cs
@@ -3,17 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CarSareMerged.Models;
 
 namespace CarSareMerged.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
             return View();
         }
+        public ActionResult Login()
+        {
+            return View();
+        }
 
-        public ActionResult About()
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Member member)
+        {
+            if (ModelState.IsValid)
+            {
+                    var obj = _context.Members.Where(a => a.Email.Equals(member.Email) && a.Password.Equals(member.Password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UserID"] = obj.ID.ToString();
+                        Session["Email"] = obj.Email.ToString();
+                        return RedirectToAction("UserDashBoard");
+                    }
+            }
+            return View(member);
+        }
+          
+        
+
+        public ActionResult UserDashBoard()
+        {
+            if (Session["UserID"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
+        }
+    
+
+public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
